0cbbd000d432045d03d136525cc3d54eabcf0dd8
gustavogana <gagana@fullsail.edu>
2015-06-17 10:06:08 -0400

    added var

--- Gana_Gustavo_Conditionals_Assignment/JS/Script.js
+++ Gana_Gustavo_Conditionals_Assignment/JS/Script.js
@@ -1,5 +1,10 @@
-/**
- * Created by GustavoGana on 6/17/15.
- */
-//Conditionals Assignment
-
+//Gana_Gustavo_Conditionals_Assignment
+//06/17/2015
+var age = prompt("PLease enter age");
+if (age < 21){
+   console.log("Congratulations!! You are old enough to board the ship");
+}
+alert("Congratulations!! You are old enough to board the ship")
+var height = prompt("PLease enter height");
+var weight = prompt("PLease enter weight");
+var sex = prompt("Please enter sex");

b549fcfd2ff9ea0e4c21308e73a97af2e692bf3f
gustavogana <gagana@fullsail.edu>
2015-06-17 17:51:13 -0400

    mad a new assignment

--- Gana_Gustavo_Conditionals_Assignment/JS/Script.js
+++ Gana_Gustavo_Conditionals_Assignment/JS/Script.js
@@ -1,6 +1,28 @@
 //Gana_Gustavo_Conditionals_Assignment
 //06/17/2015
 var oldEnough = prompt("How old are you?");
-console.log(oldEnough);
-var height = prompt("How tall are uou in inches?");
+console.log(" your age is " + oldEnough);
+var minAge = 18;
+if(oldEnough >= minAge){
+    console.log("Welcome to military");
+}else{
+    console.log("Try again when your old enough");
+}
+
+var height = prompt("How tall are you in inches?");
+var minHeight = 70;
+console.log("You are " + height + " inches tall");
+if(height >= minHeight){
+    console.log("You can be an infantry man");
+}else{
+    console.log("Desk job for you");
+}
+
 var weight = prompt("How much do you weigh?");
+var maxWeight = 185;
+console.log("You weigh "+ weight + " lbs");
+if(weight <= maxWeight){
+    console.log("You can fly planes");
+}else{
+    console.log("The plane will not fly!!");
+}

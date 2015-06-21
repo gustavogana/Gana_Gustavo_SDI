2309e5657f6c0f34b0099c8ec2ad7e98c7890b4a
gustavogana <gagana@fullsail.edu>
2015-06-21 18:44:48 -0400

    following along

---
+++ Functions/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Created by GustavoGana on 6/21/15.
+ */
+function outptMsg(){
+    console.log("hello world");
+}
+calcArea(30, 20)
+function calcArea(w, h){
+    var area = w * h;
+    console.log(area);
+}
+function dogYears(age){
+    var dogYears = age * 7;
+    console.log("sparky is " + dogYears + " years old.")
+}
+var age1 = 4;
+dogYears(age1);
+dogYears(5);

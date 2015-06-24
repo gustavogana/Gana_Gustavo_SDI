0901f62ee7947a3d0da40b8d126bd5fa8517b2af
gustavogana <gagana@fullsail.edu>
2015-06-24 18:15:52 -0400

    adjusting

--- Gana_Gustavo_Functions_Assignments/JS/script.js
+++ Gana_Gustavo_Functions_Assignments/JS/script.js
@@ -1,3 +1,10 @@
 /**
  * Created by GustavoGana on 6/24/15.
  */
+function flight(){
+    var time = prompt("How fast are you going?");
+    var length = prompt("How far is your destination?");
+    var speed = length / time;
+    console.log(speed);
+}
+flight();

1a89f686862e10d33552d1de04cb7127488e0190
gustavogana <gagana@fullsail.edu>
2015-06-25 18:09:51 -0400

    adding functions

--- Gana_Gustavo_Functions_Assignments/JS/script.js
+++ Gana_Gustavo_Functions_Assignments/JS/script.js
@@ -11,7 +11,7 @@ var multi2 = prompt("Please enter second number to multiply by");
     console.log("by " + multi2);
 while(multi2==="" || isNaN(multi2)){
 
-        multi2 = prompt("PLease do not leave blank and only number \nWhat number would you like to multiply?");
+        multi2 = prompt("Please do not leave blank and only number \nWhat number would you like to multiply?");
     }
 var answerM = multiplication * multi2;
     console.log("Your answer is " + answerM);
@@ -25,4 +25,17 @@ lightYears(answerM);
 
 var age = prompt("How old are you?");
     console.log("You are " + age + " years old");
-if( )
+while(age==="" || isNaN(age)){
+    age = prompt("Don't you have an age? and letters do not count, \nnumbers only")
+}
+if (age <= 45){
+    console.log("You're still young");
+    alert("Your still young");
+}else{
+    console.log("You had better days")
+    alert("You had better days");
+}
+function ageTotal(yearsPassed){
+    var ageTotal = yearsPassed * age;
+    console.log(agr)
+}

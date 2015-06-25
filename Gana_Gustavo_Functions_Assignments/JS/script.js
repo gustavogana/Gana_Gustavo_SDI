40a62ee0512506637e1de0466a4cca633b9f21dd
gustavogana <gagana@fullsail.edu>
2015-06-24 20:14:14 -0400

    trying out different ideas

--- Gana_Gustavo_Functions_Assignments/JS/script.js
+++ Gana_Gustavo_Functions_Assignments/JS/script.js
@@ -1,8 +1,20 @@
 /**
  * Created by GustavoGana on 6/24/15.
  */
-var b = 10;
-while (b > 0){
-    console.log(b);
+var multiplication = prompt("Enter a number to multiply by");
+    console.log("you choose " + multiplication + " to multiply");
+while(multiplication==="" || isNaN(multiplication)) {
+    if(multiplication===""){
+        multiplication = prompt("Please do not leave blank and only use numbers \nWhat number would you like to multiply?");
+        multiplication = parseInt(multiplication);
 }
-
+var multi2 = prompt("Please enter second number to multiply by");
+    console.log("by " + multi2);
+while(multi2==="" || isNaN(multi2)){
+    if (multi2 === ""){}
+}
+        multi2 = prompt("PLease do not leave blank and only number \nWhat number would you like to multiply?");
+        multi2 = parseInt(multi2);
+    }
+var answerM = multiplication * multi2;
+    console.log(answerM);

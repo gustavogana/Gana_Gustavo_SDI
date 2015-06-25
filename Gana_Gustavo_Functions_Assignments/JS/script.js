2674d797a4943662f0d01125ba505f80e617966d
gustavogana <gagana@fullsail.edu>
2015-06-25 17:54:00 -0400

    Functions added

--- Gana_Gustavo_Functions_Assignments/JS/script.js
+++ Gana_Gustavo_Functions_Assignments/JS/script.js
@@ -1,20 +1,28 @@
-/**
+/*
  * Created by GustavoGana on 6/24/15.
  */
 var multiplication = prompt("Enter a number to multiply by");
     console.log("you choose " + multiplication + " to multiply");
-while(multiplication==="" || isNaN(multiplication)) {
-    if(multiplication===""){
+while(multiplication==="" || isNaN(multiplication)){
+
         multiplication = prompt("Please do not leave blank and only use numbers \nWhat number would you like to multiply?");
-        multiplication = parseInt(multiplication);
 }
 var multi2 = prompt("Please enter second number to multiply by");
     console.log("by " + multi2);
 while(multi2==="" || isNaN(multi2)){
-    if (multi2 === ""){}
-}
+
         multi2 = prompt("PLease do not leave blank and only number \nWhat number would you like to multiply?");
-        multi2 = parseInt(multi2);
     }
 var answerM = multiplication * multi2;
-    console.log(answerM);
+    console.log("Your answer is " + answerM);
+alert("Your answer is " + answerM);
+
+function lightYears(distance){
+    var lightYears = distance * 31556926;
+    console.log("You traveled " + lightYears + " light years")
+}
+lightYears(answerM);
+
+var age = prompt("How old are you?");
+    console.log("You are " + age + " years old");
+if( )
